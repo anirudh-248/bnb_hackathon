57f4980bb4b7459c8cf6edf34387c1e9eae87888
anirudh-248 <anirudhphaniraj1@gmail.com>
2024-09-30 00:11:44 +0530

    updated database

--- myproject/settings.py
+++ myproject/settings.py
@@ -75,10 +75,28 @@ WSGI_APPLICATION = "myproject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'vwdb',
+
+        'USER': 'anirudh',
+
+        'PASSWORD': 'anirudh_248',
+
+        'HOST': 'vw-db.c10s0ug4k4kk.ap-southeast-2.rds.amazonaws.com',
+
+        'PORT': '5432',
     }
 }
 
